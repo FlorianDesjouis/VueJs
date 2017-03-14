ad2e11fc77a6ee24fa406581414f5e3af58734bd
Edison Services <florian.desjouis@supinternet.fr>
2017-03-14 14:21:09 +0100

    fixed filter

--- filterVueJS/script.js
+++ filterVueJS/script.js
@@ -2,9 +2,18 @@ new Vue({
 	el: '.content',
 	data: {
 		message: "Hey Pelo"
+	},
+	filters: {
+		uppercase: function (value) {
+			if(!value) return ''
+				value = value.toString()
+			return value.toUpperCase()
+		},
+		capitalize: function (value) {
+			if(!value) return ''
+				value = value.toString()
+			return value.charAt(0).toUpperCase() + value.slice(1)
+		}
 	}
 })
 
-Vue.filter('uppercase', function(value) {
-	return result.toUpperCase()
-})
